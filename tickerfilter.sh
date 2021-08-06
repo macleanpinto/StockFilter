771ca8006f87f9ea11b04408a1e7ff8889e4627f
macleanpinto <macleanmauricepinto@gmail.com>
2021-08-06 09:44:39 +0530

    Update tickerfilter.sh

--- tickerfilter.sh
+++ tickerfilter.sh
@@ -11,12 +11,7 @@ echo 'symbol,short_name,change,regular_market_price,fifty_two_week_high,fifty_tw
     # through that row in the loop below
     read
      while IFS="," read -r -a arr; do
-        steampipe query "select symbol,short_name,(fifty_two_week_high-regular_market_price)*100/fifty_two_week_high as change,
-        regular_market_price,fifty_two_week_high,fifty_two_week_low,fifty_day_average,two_hundred_day_average,
-        regular_market_open,regular_market_day_high,regular_market_day_low,regular_market_previous_close,
-        regular_market_volume from finance.finance_quote 
-        where symbol = '${arr[@]:0:1}.NS' and (fifty_day_average < two_hundred_day_average) 
-        order by regular_market_price desc" --header=false --output csv >> Buy_Call_${today}.csv
+        echo ${arr[@]:0:1}.NS
 
     done
 } <../NSE_Symbols.csv
