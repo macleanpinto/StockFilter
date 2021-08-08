82cd74b8b9ef212914a2a940c2fa0414d09536a8
macleanpinto <macleanmauricepinto@gmail.com>
2021-08-08 19:53:39 +0530

    Update tickerfilter.sh

--- tickerfilter.sh
+++ tickerfilter.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+clean_up() {
+    steampipe service stop --force
+}
+trap clean_up EXIT
 
 today=$(date +"%Y-%m-%d")
 
@@ -8,14 +11,20 @@ echo 'symbol,short_name,change,regular_market_price,fifty_two_week_high,fifty_tw
 {
     # this reads the first row which has the column names so we will not go
     # through that row in the loop below
-    while IFS="," read -r sym _; do
+    read
+    while IFS="," read -r -a arr; do
+    { # try
 
     steampipe query "select symbol,short_name,(fifty_two_week_high-regular_market_price)*100/fifty_two_week_high as change,
     regular_market_price,fifty_two_week_high,fifty_two_week_low,fifty_day_average,two_hundred_day_average,
     regular_market_open,regular_market_day_high,regular_market_day_low,regular_market_previous_close,
     regular_market_volume from finance.finance_quote 
-    where symbol = '"$sym.NS"' and (fifty_day_average < two_hundred_day_average)
+    where symbol = '${arr[@]:0:1}.NS' and (fifty_day_average < two_hundred_day_average) 
     order by regular_market_price desc" --header=false --output csv >> Buy_Call_${today}.csv
     
+
+    } || { # catch
+        echo "fetch call failed for " ${arr[@]:0:1}.NS
+    }
     done
 } <../NSE_Symbols.csv
